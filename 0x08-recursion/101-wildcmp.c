79cbf8a0e4ab2919d6cd16e69256e36478632d17
Ogboh chinonso Benjamin <Ogbohchinonso@gmail.com>
2023-07-06 03:19:30 +0100

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,7 +3,7 @@
 /**
  * wildcmp - compares two strings.
  * @s1: string 1.
- * @st2: It can contains a * as a special character.
+ * @s2: It can contains a * as a special character.
  * Return: 1 if are identical, 0 if not.
  */
 
@@ -14,9 +14,8 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 +1));
+		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*')
 		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	return (0);
 }
-
